6f2a1a4de1df734bf52d67cef0abfc2cd13acf2a
Ahmed Meshkin <ahmedmeshkin@gmail.com>
2024-05-15 18:23:25 +0200

    finished making page template

--- page.php
+++ page.php
@@ -1,8 +1,13 @@
 <?php get_header() ?>
 
-<section id="page" class="h-screen">
-    <div class="flex justify-center items-center h-full w-full">
-        <h1 class="text-black font-averta text-4xl italic">This page is under development...</h1>
+<section id="pageTemplate" class="py-24 bg-white">
+    <div class="container mx-auto">
+        <div class="mx-10">
+            <h1 class="text-center text-black font-averta font-bold text-5xl mb-10"><?php the_title(); ?></h1>
+            <div class="pageContent_wrapper font-averta">
+                <?php echo the_content(); ?>
+            </div>
+        </div>
     </div>
 </section>
 
